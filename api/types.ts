d66fbf89330493034d36105b3cfa099c39013363
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-21 23:49:51 +0600

    Удалила неиспользуемые интерфейсы и типы, связанные с User

--- api/types.ts
+++ api/types.ts
@@ -1,17 +1,10 @@
-import {Model, Types} from 'mongoose';
+import {Types} from 'mongoose';
 
 export interface UserFields {
     username: string;
     password: string;
     token: string;
 }
-
-export interface UserMethods {
-    checkPassword(password: string): Promise<boolean>;
-    generateToken(): void;
-}
-
-export type UserModel = Model<UserFields, {}, UserMethods>;
 
 export interface IPost {
     user: Types.ObjectId;
