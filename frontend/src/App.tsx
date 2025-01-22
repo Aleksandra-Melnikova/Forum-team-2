53b98fac217ac45810fe84cca75de63cc74ed037
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-22 18:08:28 +0600

    [Фронтенд] Импортировала комопоненты, связанные с сущностью Post и создала для них роуты

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,18 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import RegisterPage from "./features/users/RegisterPage.tsx";
 import LoginPage from "./features/users/LoginPage.tsx";
+import PostForm from "./features/posts/PostForm.tsx";
+import Posts from "./features/posts/Posts.tsx";
+import DetailedPost from "./features/posts/DetailedPost.tsx";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/add-post" element={<PostForm />} />
+        <Route path="/posts/:postId" element={<DetailedPost />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
