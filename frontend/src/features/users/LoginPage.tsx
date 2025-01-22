99338f03cda2f6992dee12710be52430baf825bc
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-01-22 17:04:07 +0600

    [Фронтенд] Удалила background из компонентов LoginPage.tsx и RegisterPage.tsx

--- frontend/src/features/users/LoginPage.tsx
+++ frontend/src/features/users/LoginPage.tsx
@@ -34,7 +34,7 @@ const Register = () => {
   };
 
   return (
-    <section className="vh-100 bg-body-secondary">
+    <section className="vh-100">
       <div className="container h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-lg-12 col-xl-11">
