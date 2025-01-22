a4a2d6fa80b4146398522449c6ee1bbf03e42a4b
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-01-22 09:22:20 +0600

    FT3-6 [Фронтенд] Создала компонент RegisterPage и LoginPage. Были  прописаны и подключены запросы.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <ToastContainer />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+);
