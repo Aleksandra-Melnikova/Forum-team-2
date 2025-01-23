cf219fa7d80d18064ffb4d7db619b0b70d1f7618
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-23 18:41:24 +0600

    [Фронтенд] Добавила и отобразила лоадер

--- frontend/src/features/posts/Posts.tsx
+++ frontend/src/features/posts/Posts.tsx
@@ -1,26 +1,34 @@
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
-import { selectPosts } from "./PostSlice.ts";
+import { selectFetching, selectPosts } from "./PostSlice.ts";
 import { useEffect } from "react";
 import { fetchPosts } from "./PostThunk.ts";
 import { IPost } from "../../types";
 import Post from "./Post.tsx";
+import Loader from "../../components/UI/Loader/Loader.tsx";
 
 const Posts = () => {
   const dispatch = useAppDispatch();
   const posts = useAppSelector(selectPosts);
+  const isLoading = useAppSelector(selectFetching);
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, [dispatch]);
 
   return (
-    <div className="d-flex flex-column align-items-center mt-5">
-      <>
-        {posts.map((post: IPost) => (
-          <Post post={post} key={post._id} />
-        ))}
-      </>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="d-flex flex-column align-items-center mt-5">
+          <>
+            {posts.map((post: IPost) => (
+              <Post post={post} key={post._id} />
+            ))}
+          </>
+        </div>
+      )}
+    </>
   );
 };
 
