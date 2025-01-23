8ff59f3078b80e6c78bd6bc892351ba0cfbfcb43
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-23 18:40:47 +0600

    [Фронтенд] 1. Добавила иконку, которая отображается на месте изображения, если оно отсутствует 2. Отобразила лоадер

--- frontend/src/features/posts/DetailedPost.tsx
+++ frontend/src/features/posts/DetailedPost.tsx
@@ -1,14 +1,16 @@
-import { selectOnePost } from "./PostSlice.ts";
+import { selectFetching, selectOnePost } from "./PostSlice.ts";
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
 import { useEffect } from "react";
 import { getPost } from "./PostThunk.ts";
 import dayjs from "dayjs";
 import { useParams } from "react-router-dom";
 import { apiUrl } from "../../globalConstants.ts";
+import Loader from "../../components/UI/Loader/Loader.tsx";
 
 const DetailedPost = () => {
-  const post = useAppSelector(selectOnePost);
   const dispatch = useAppDispatch();
+  const post = useAppSelector(selectOnePost);
+  const isLoading = useAppSelector(selectFetching);
   const { postId } = useParams();
 
   useEffect(() => {
@@ -19,28 +21,46 @@ const DetailedPost = () => {
 
   return (
     post && (
-      <div className="container mt-5">
-        <div className="my-5 shadow rounded">
-          <div className="card shadow-sm rounded border-0">
-            <div className="card-body">
-              <h3>{post.user.username}</h3>
-              <hr />
-              <h5 className="card-title my-4">{post.title}</h5>
-              <img
-                src={post.image ? `${apiUrl}/${post.image}` : undefined}
-                alt={post.title}
-                className="w-25 card-img mb-4"
-              />
-              <p className="card-text">
-                {post.description ? post.description : null}
-              </p>
-              <p className="text-muted">
-                {dayjs(post.datetime).format("DD.MM.YYYY, в HH:mm")}
-              </p>
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="container mt-5">
+            <div className="my-5 shadow rounded">
+              <div className="card shadow-sm rounded border-0">
+                <div className="card-body">
+                  <h3>{post.user.username}</h3>
+                  <hr />
+                  <h5 className="fw-semibold fs-4 my-2">{post.title}</h5>
+                  <div className="row d-flex mt-2">
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center text-primary">
+                      {post.image ? (
+                        <img
+                          src={
+                            post.image ? `${apiUrl}/${post.image}` : undefined
+                          }
+                          alt={post.title}
+                          className="w-100 h-auto mb-3 rounded"
+                        />
+                      ) : (
+                        <i className="bi bi-chat-left-text-fill"></i>
+                      )}
+                    </div>
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12 d-flex flex-column">
+                      <p className=" flex-grow-1">
+                        {post.description ? post.description : null}
+                      </p>
+                      <p className="text-muted">
+                        {dayjs(post.datetime).format("DD.MM.YYYY, в HH:mm")}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        )}
+      </>
     )
   );
 };
